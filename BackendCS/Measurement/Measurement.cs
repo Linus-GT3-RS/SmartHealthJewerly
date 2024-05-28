1f51e68dcd655c2c51595df5a1cd01cc3cc33279
ewegele <eduard.wegele1511@gmail.com>
2024-05-28 20:53:31 +0200

    implement backend infrastructure

---
+++ BackendCS/Measurement/Measurement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCS.Measurement
+{
+    internal class Measurement
+    {
+        private Motion _motion;
+        private HeartRate _heartRate;
+        private Humidity _humidity;
+        private Temperature _temperature;
+    }
+
+}
---
+++ BackendCS/Patient/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCS
+{
+    internal class Patient
+    {
+        private string _firstname;
+        private string _lastname;
+        private int _age;
+
+    }
+}
