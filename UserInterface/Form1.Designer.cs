a595ccea34a96809befcc0a38853f0119fb873e0
Javaxime <maxime.riebschl@gmail.com>
2024-05-30 22:16:39 +0200

    Projekt rüberkopiert(err mit import)

--- UserInterface/Form1.Designer.cs
+++ UserInterface/Form1.Designer.cs
@@ -1,40 +1,158 @@
-﻿namespace UserInterface
+﻿using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace GUI_Toto
 {
-    partial class Form1
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
 
-        #region Windows Form Designer generated code
+      #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+         System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+         System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+         System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+         this.HerzschlagSpannungsLabel = new System.Windows.Forms.Label();
+         this.luftfeuchtigkeitsLabel = new System.Windows.Forms.Label();
+         this.temperaturLabel = new System.Windows.Forms.Label();
+         this.BPMLabel = new System.Windows.Forms.Label();
+         this.dully = new System.Windows.Forms.Label();
+         this.KoerperTemperaturLabel = new System.Windows.Forms.Label();
+         this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+         ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // HerzschlagSpannungsLabel
+         // 
+         this.HerzschlagSpannungsLabel.AutoSize = true;
+         this.HerzschlagSpannungsLabel.Location = new System.Drawing.Point(31, 39);
+         this.HerzschlagSpannungsLabel.Name = "HerzschlagSpannungsLabel";
+         this.HerzschlagSpannungsLabel.Size = new System.Drawing.Size(51, 20);
+         this.HerzschlagSpannungsLabel.TabIndex = 0;
+         this.HerzschlagSpannungsLabel.Text = "label1";
+         // 
+         // luftfeuchtigkeitsLabel
+         // 
+         this.luftfeuchtigkeitsLabel.AutoSize = true;
+         this.luftfeuchtigkeitsLabel.Location = new System.Drawing.Point(35, 92);
+         this.luftfeuchtigkeitsLabel.Name = "luftfeuchtigkeitsLabel";
+         this.luftfeuchtigkeitsLabel.Size = new System.Drawing.Size(51, 20);
+         this.luftfeuchtigkeitsLabel.TabIndex = 1;
+         this.luftfeuchtigkeitsLabel.Text = "label1";
+         // 
+         // temperaturLabel
+         // 
+         this.temperaturLabel.AutoSize = true;
+         this.temperaturLabel.Location = new System.Drawing.Point(35, 148);
+         this.temperaturLabel.Name = "temperaturLabel";
+         this.temperaturLabel.Size = new System.Drawing.Size(51, 20);
+         this.temperaturLabel.TabIndex = 2;
+         this.temperaturLabel.Text = "label2";
+         // 
+         // BPMLabel
+         // 
+         this.BPMLabel.AutoSize = true;
+         this.BPMLabel.Location = new System.Drawing.Point(190, 39);
+         this.BPMLabel.Name = "BPMLabel";
+         this.BPMLabel.Size = new System.Drawing.Size(51, 20);
+         this.BPMLabel.TabIndex = 3;
+         this.BPMLabel.Text = "label1";
+         // 
+         // dully
+         // 
+         this.dully.AutoSize = true;
+         this.dully.Location = new System.Drawing.Point(194, 114);
+         this.dully.Name = "dully";
+         this.dully.Size = new System.Drawing.Size(51, 20);
+         this.dully.TabIndex = 4;
+         this.dully.Text = "label1";
+         // 
+         // KoerperTemperaturLabel
+         // 
+         this.KoerperTemperaturLabel.AutoSize = true;
+         this.KoerperTemperaturLabel.Location = new System.Drawing.Point(185, 181);
+         this.KoerperTemperaturLabel.Name = "KoerperTemperaturLabel";
+         this.KoerperTemperaturLabel.Size = new System.Drawing.Size(51, 20);
+         this.KoerperTemperaturLabel.TabIndex = 5;
+         this.KoerperTemperaturLabel.Text = "label1";
+         // 
+         // chart1
+         // 
+         chartArea1.Name = "ChartArea1";
+         this.chart1.ChartAreas.Add(chartArea1);
+         legend1.Name = "Legend1";
+         this.chart1.Legends.Add(legend1);
+         this.chart1.Location = new System.Drawing.Point(349, 27);
+         this.chart1.Name = "chart1";
+         series1.ChartArea = "ChartArea1";
+         series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+         series1.Legend = "Legend1";
+         series1.Name = "HeartRate";
+         series1.XAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+         series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+         this.chart1.Series.Add(series1);
+         this.chart1.Size = new System.Drawing.Size(602, 308);
+         this.chart1.TabIndex = 6;
+         this.chart1.Text = "chart1";
+         title1.Name = "Title1";
+         this.chart1.Titles.Add(title1);
+         chart1.ChartAreas[0].AxisY.Minimum = 600; // Set the minimum y-axis value
+         chart1.ChartAreas[0].AxisY.Maximum = 800; // Set the maximum y-axis value
+         chart1.ChartAreas[0].AxisX.Minimum = 0; // Set the minimum x-axis value
+         chart1.ChartAreas[0].AxisX.Maximum = 100; // Set the maximum x-axis value
 
-        #endregion
-    }
+         // 
+         // Form1
+         // 
+         this.ClientSize = new System.Drawing.Size(991, 441);
+         this.Controls.Add(this.chart1);
+         this.Controls.Add(this.KoerperTemperaturLabel);
+         this.Controls.Add(this.dully);
+         this.Controls.Add(this.BPMLabel);
+         this.Controls.Add(this.temperaturLabel);
+         this.Controls.Add(this.luftfeuchtigkeitsLabel);
+         this.Controls.Add(this.HerzschlagSpannungsLabel);
+         this.Name = "Form1";
+         ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Label HerzschlagSpannungsLabel;
+      private System.Windows.Forms.Label luftfeuchtigkeitsLabel;
+      private System.Windows.Forms.Label temperaturLabel;
+      private System.Windows.Forms.Label BPMLabel;
+      private System.Windows.Forms.Label dully;
+      private System.Windows.Forms.Label KoerperTemperaturLabel;
+      private Chart chart1;
+   }
 }
 
--- UserInterface/Form1.cs
+++ UserInterface/Form1.cs
@@ -3,18 +3,252 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UserInterface
+using System.Windows.Forms;
+using System.IO.Ports;
+
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Timers;
+
+namespace GUI_Toto
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+   public partial class Form1 : Form
+   {
+      private bool handleCreated = false;
+
+
+      private SerialPort serialPort;
+      private static readonly object _lock = new object();
+      static int heartRateBPM = 0;
+      static int counter = 0;
+      int HerzschlagCounter = 0;
+      static int beatMsec = 0;
+      const int delayMsec = 60;
+      static int maxValue = 0;
+      static bool isPeak = false;
+
+
+
+
+      public Form1()
+      {
+         InitializeComponent();
+
+         //test funktion fürs plotten
+         //Create a new thread and specify the method to be executed
+         Thread newThread = new Thread(new ThreadStart(testfuncPlotting));
+
+         // Start the thread
+         //newThread.Start();
+
+         Series series = chart1.Series["HeartRate"];
+         if (series.Points.Count > 100)
+         {
+            series.Points.RemoveAt(0); // Keep the number of points reasonable
+         }
+
+         series.Points.AddY(700);
+         chart1.Invalidate(); // Redraw the chart
+
+
+         // The main thread continues execution independently of the new thread
+         Console.WriteLine("Main thread is still running.");
+
+         // Optionally, wait for the new thread to finish
+
+
+      }
+
+
+
+      public void testfuncPlotting()
+      {
+         while (true)
+         {
+            lock (_lock)
+            {
+               Series series = chart1.Series["HeartRate"];
+               if (series.Points.Count > 100)
+               {
+                  series.Points.RemoveAt(0); // Keep the number of points reasonable
+               }
+               Random random = new Random();
+
+               // Generate a random integer between 600 and 800
+               int randomNumber = random.Next(600, 801);
+               this.Invoke(new MethodInvoker(delegate
+               {
+                  series.Points.AddY(randomNumber);
+                  chart1.Invalidate(); // Redraw the chart
+               }));
+
+               Thread.Sleep(10);
+            }
+
+         }
+
+      }
+
+
+      private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+      {
+
+         lock (_lock)
+         {
+            // Casten des Senders zurück zu SerialPort
+            SerialPort serialPort = (SerialPort)sender;
+
+            // Daten lesen und auf der Konsole anzeigen
+            string data = serialPort.ReadLine().Trim();
+
+            // Daten in einzelne Werte trennen (CSV)
+            string[] values = data.Split(',');
+            if (values.Length >= 4)
+            {
+
+               int spannungVomHerzschlagSensor = int.Parse(values[0]);
+               //herzschlagfunktionalität
+               if (HerzschlagCounter >= 6)
+               {
+                  Herzschlag(spannungVomHerzschlagSensor);
+                  HerzschlagCounter = 0;
+                  Series series = chart1.Series["HeartRate"];
+                  if (series.Points.Count > 100)
+                  {
+                     series.Points.RemoveAt(0); // Keep the number of points reasonable
+                  }
+
+                  series.Points.AddY(spannungVomHerzschlagSensor);
+                  chart1.Invalidate(); // Redraw the chart
+               }
+               HerzschlagCounter++;
+               // Invoke UI updates on the main thread
+               this.Invoke(new MethodInvoker(delegate
+               {
+                  BPMLabel.Text = heartRateBPM + " BPM";
+                  HerzschlagSpannungsLabel.Text = spannungVomHerzschlagSensor + "";
+
+                  if (values[2] != "nan")
+                  {
+                     float luftfeuchtigkeitInProzent = float.Parse(values[2]);
+                     luftfeuchtigkeitsLabel.Text = luftfeuchtigkeitInProzent + "% Luftfeuchtigkeit";
+                  }
+
+                  if (values[1] != "nan")
+                  {
+                     float WaermeInGradCelcius = float.Parse(values[1]);
+                     temperaturLabel.Text = WaermeInGradCelcius + "";
+                  }
+
+                  if (values[3] != "nan")
+                  {
+                     float KoerperTemperaturInGradCelcius = float.Parse(values[3]);
+                     KoerperTemperaturLabel.Text = KoerperTemperaturInGradCelcius + "";
+                  }
+               }));
+            }
+         }
+      }
+
+
+
+      private void initializeSerialPort()
+      {
+         serialPort = new SerialPort("COM4", 115200); // Ändern Sie COM3 entsprechend dem Anschluss des Arduinos
+         serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+         try
+         {
+            serialPort.Open();
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Fehler beim Öffnen der seriellen Verbindung: " + ex.Message);
+         }
+      }
+
+
+      //event wird getriggert, wenn apllication geschlossen wird => serialport schließen
+      private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+      {
+         // Schließe die serielle Verbindung, wenn das Formular geschlossen wird
+         if (serialPort.IsOpen)
+         {
+            serialPort.Close();
+         }
+      }
+
+
+
+      public void Herzschlag(int Spannung)
+      {
+         if (heartbeatDetected(delayMsec, Spannung))
+         {
+            if (beatMsec != 0)
+            {
+               heartRateBPM = 60000 / beatMsec;
+            }
+
+
+            beatMsec = 0;
+         }
+         beatMsec += delayMsec;
+
+         //dully.Text = getDurchschnittLast20Spannungen() + "";
+         //HerzschlagCounter++;
+      }
+
+
+
+      bool heartbeatDetected(int delay, int rawValue)
+      {
+
+         bool result = false;
+
+         // Here the current voltage value at the photo transistor is read out and stored temporarily in the rawValue variable
+         rawValue *= (1000 / delay);
+
+         // Should the current value deviate too far from the last maximum value
+         // (e.g. because the finger was put on again or taken away)
+         // So the MaxValue is reset to get a new base.
+         if (rawValue * 4L < maxValue)
+         {
+            maxValue = (int)(rawValue * 0.8);
+         } // Detect new peak
+         if (rawValue > maxValue - (1000 / delay))
+         {
+            // The actual peak is detected here. Should a new RawValue be bigger
+            // as the last maximum value, it will be recognized as the top of the recorded data.
+            if (rawValue > maxValue)
+            {
+               maxValue = rawValue;
+            }
+            // Only one heartbeat should be assigned to the recognized peak
+            if (isPeak == false)
+            {
+               result = true;
+            }
+            isPeak = true;
+         }
+         else if (rawValue < maxValue - (3000 / delay))
+         {
+            isPeak = false;
+            // This is the maximum value for each pass
+            // slightly reduced again. The reason for this is that
+            // not only the value is otherwise always stable with every stroke
+            // would be the same or smaller, but also,
+            // if the finger should move minimally and thus
+            // the signal would generally become weaker.
+            maxValue -= (1000 / delay);
+         }
+         return result;
+      }
+
+   }
 }
