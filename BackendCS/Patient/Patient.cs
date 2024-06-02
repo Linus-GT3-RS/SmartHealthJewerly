3990c7555438feb2a7e1661ff6c55f3fa3c12a96
ewegele <eduard.wegele1511@gmail.com>
2024-06-02 14:09:49 +0200

    Notification is now implemented in C#, Pythoncode was deleted

--- BackendCS/Patient/Patient.cs
+++ BackendCS/Patient/Patient.cs
@@ -11,6 +11,21 @@ namespace BackendCS
         private string _firstname;
         private string _lastname;
         private int _age;
+        private List<ContactPerson> _contacts;
+        private HealthData _healthData;
 
+        Patient(string firstname, string lastname, int age, List<ContactPerson> contact, HealthData healthData)
+        {
+                _firstname = firstname;
+                _lastname = lastname;
+                _age = age;
+                _contacts = contact;
+                _healthData = healthData;
+        }
+
+        public void setContactPerson(ContactPerson contact)
+        {
+            _contacts.Add(contact);
+        }
     }
 }
--- BackendCS/Patient/Profile.cs
+++ BackendCS/Patient/Profile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,23 @@ namespace BackendCS
     {
         private string _loginname;
         private string _password;
-        private Patient[] _patients;
+        private List<Patient> _patients;
+
+        private Profile(string loginname, string password, List<Patient> patients)
+        {
+            _loginname = loginname;
+            _password = password;
+            _patients = patients;
+        }
+
+        public static Profile CreateProfile(string loginname, string password, List<Patient> patients)
+        {
+            return new Profile(loginname,password,patients);
+        }
+
+        public void AddProfile(Patient patient)
+        {
+            _patients.Add(patient);
+        }
     }
 }
