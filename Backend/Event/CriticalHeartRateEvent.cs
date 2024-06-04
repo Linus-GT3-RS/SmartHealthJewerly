3af437feea249ae636385dae03308473d4bb9d7e
ewegele <eduard.wegele1511@gmail.com>
2024-06-05 00:57:18 +0200

    add CriticialHeartRateEvent

--- Backend/Event/CriticalHeartRateEvent.cs
+++ Backend/Event/CriticalHeartRateEvent.cs
@@ -8,5 +8,29 @@ namespace BackendCS.Event
 {
     internal class CriticalHeartRateEvent
     {
+        public delegate void CriticalHeartRateHandler(CriticalHeartRateEventArgs e);
+
+        public event CriticalHeartRateHandler CriticialHeartRate; // callback Function
+
+        public void Check(int i)
+        {
+            // logic of checking heartrate
+
+            if (CriticialHeartRate != null)
+            {
+                // löst Event aus
+                CriticialHeartRate(new CriticalHeartRateEventArgs("kritischer Wert wurde überschritten"));
+            }
+        }
+    }
+
+    public class CriticalHeartRateEventArgs
+    {
+        public string Message { get; private set; }
+
+        public CriticalHeartRateEventArgs(string message)
+        {
+            Message = message;
+        }
     }
 }
