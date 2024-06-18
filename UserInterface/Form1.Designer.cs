39a015103fb306523b7f70d51b9461a25869f4f9
Dennis Apelt <dennis.apelt.da@gmail.com>
2024-06-18 18:36:50 +0200

    chart included in gui

--- UserInterface/Form1.Designer.cs
+++ UserInterface/Form1.Designer.cs
@@ -31,6 +31,9 @@ namespace UserInterface
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.buttonStart = new System.Windows.Forms.Button();
             this.labelBPM = new System.Windows.Forms.Label();
             this.labelBodyTemp = new System.Windows.Forms.Label();
@@ -41,23 +44,25 @@ namespace UserInterface
             this.labelMotionAccY = new System.Windows.Forms.Label();
             this.labelMotionAccZ = new System.Windows.Forms.Label();
             this.buttonStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(118, 68);
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(58, 42);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(178, 80);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "Start Measurement";
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
-            this.buttonStart.Enabled = false;
             // 
             // labelBPM
             // 
             this.labelBPM.AutoSize = true;
-            this.labelBPM.Location = new System.Drawing.Point(467, 68);
+            this.labelBPM.Location = new System.Drawing.Point(32, 317);
             this.labelBPM.Name = "labelBPM";
             this.labelBPM.Size = new System.Drawing.Size(186, 25);
             this.labelBPM.TabIndex = 1;
@@ -66,7 +71,7 @@ namespace UserInterface
             // labelBodyTemp
             // 
             this.labelBodyTemp.AutoSize = true;
-            this.labelBodyTemp.Location = new System.Drawing.Point(874, 123);
+            this.labelBodyTemp.Location = new System.Drawing.Point(32, 524);
             this.labelBodyTemp.Name = "labelBodyTemp";
             this.labelBodyTemp.Size = new System.Drawing.Size(194, 25);
             this.labelBodyTemp.TabIndex = 2;
@@ -75,7 +80,7 @@ namespace UserInterface
             // labelEnvTemp
             // 
             this.labelEnvTemp.AutoSize = true;
-            this.labelEnvTemp.Location = new System.Drawing.Point(467, 123);
+            this.labelEnvTemp.Location = new System.Drawing.Point(32, 372);
             this.labelEnvTemp.Name = "labelEnvTemp";
             this.labelEnvTemp.Size = new System.Drawing.Size(265, 25);
             this.labelEnvTemp.TabIndex = 3;
@@ -84,7 +89,7 @@ namespace UserInterface
             // labelHumidity
             // 
             this.labelHumidity.AutoSize = true;
-            this.labelHumidity.Location = new System.Drawing.Point(467, 176);
+            this.labelHumidity.Location = new System.Drawing.Point(32, 425);
             this.labelHumidity.Name = "labelHumidity";
             this.labelHumidity.Size = new System.Drawing.Size(230, 25);
             this.labelHumidity.TabIndex = 4;
@@ -93,7 +98,7 @@ namespace UserInterface
             // labelBrightness
             // 
             this.labelBrightness.AutoSize = true;
-            this.labelBrightness.Location = new System.Drawing.Point(467, 226);
+            this.labelBrightness.Location = new System.Drawing.Point(32, 475);
             this.labelBrightness.Name = "labelBrightness";
             this.labelBrightness.Size = new System.Drawing.Size(126, 25);
             this.labelBrightness.TabIndex = 5;
@@ -102,7 +107,7 @@ namespace UserInterface
             // labelMotionAccX
             // 
             this.labelMotionAccX.AutoSize = true;
-            this.labelMotionAccX.Location = new System.Drawing.Point(467, 373);
+            this.labelMotionAccX.Location = new System.Drawing.Point(32, 567);
             this.labelMotionAccX.Name = "labelMotionAccX";
             this.labelMotionAccX.Size = new System.Drawing.Size(139, 25);
             this.labelMotionAccX.TabIndex = 6;
@@ -111,24 +116,24 @@ namespace UserInterface
             // labelMotionAccY
             // 
             this.labelMotionAccY.AutoSize = true;
-            this.labelMotionAccY.Location = new System.Drawing.Point(709, 373);
+            this.labelMotionAccY.Location = new System.Drawing.Point(277, 567);
             this.labelMotionAccY.Name = "labelMotionAccY";
             this.labelMotionAccY.Size = new System.Drawing.Size(140, 25);
             this.labelMotionAccY.TabIndex = 7;
             this.labelMotionAccY.Text = "MotionAccY: ";
             // 
-            // MotionAccZ
+            // labelMotionAccZ
             // 
             this.labelMotionAccZ.AutoSize = true;
-            this.labelMotionAccZ.Location = new System.Drawing.Point(945, 373);
-            this.labelMotionAccZ.Name = "MotionAccZ";
+            this.labelMotionAccZ.Location = new System.Drawing.Point(510, 567);
+            this.labelMotionAccZ.Name = "labelMotionAccZ";
             this.labelMotionAccZ.Size = new System.Drawing.Size(138, 25);
             this.labelMotionAccZ.TabIndex = 8;
             this.labelMotionAccZ.Text = "MotionAccZ: ";
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(118, 258);
+            this.buttonStop.Location = new System.Drawing.Point(58, 151);
             this.buttonStop.Name = "buttonStop";
             this.buttonStop.Size = new System.Drawing.Size(178, 72);
             this.buttonStop.TabIndex = 9;
@@ -136,9 +141,26 @@ namespace UserInterface
             this.buttonStop.UseVisualStyleBackColor = true;
             this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(446, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(844, 468);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1338, 623);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.buttonStop);
             this.Controls.Add(this.labelMotionAccZ);
             this.Controls.Add(this.labelMotionAccY);
@@ -150,6 +172,7 @@ namespace UserInterface
             this.Controls.Add(this.labelBPM);
             this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,6 +191,7 @@ namespace UserInterface
         private Label labelMotionAccY;
         private Label labelMotionAccZ;
         private Button buttonStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
