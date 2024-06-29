b8d0092c6ffb70b87da5f5ac202cd1ce745fb91d
Linus <dotexelhd@gmail.com>
2024-06-29 10:54:22 +0200

    added LoginSuccess event

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -17,7 +17,7 @@ namespace UserInterface
     {
         // ---------------------------------- Start CONFIG : Free to use ----------------------------------
 
-        static RunSettings CurRunSettings = RunSettings.Normal;
+        static RunSettings CurRunSettings = RunSettings.LogIn_Only;
 
         // ---------------------------------- END CONFIG -----------------------------------------------------
 
